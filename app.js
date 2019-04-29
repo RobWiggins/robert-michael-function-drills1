1f702f3ddcba6c94c46d000e790ff3c634729918
m-cal <iengineersoftware@gmail.com>
2019-04-29 16:14:50 -0400

    completed up to step 7(1) of function drills 1

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 'use strict';
 function getYearOfBirth(age) {
+    if (age < 0) {
+        throw new Error('Age cannot be negative!');
+    }
     return 2019 - age;
 }
 
+
+
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and my age is ${age} and I was born in ${getYearOfBirth(age)}.`;
+    try {
+        let yearOfBirth = getYearOfBirth(age);
+        return `Hi, my name is ${name} and my age is ${age} and I was born in ${yearOfBirth}.`;
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 const aSentence = createGreeting('Mike', 23);
