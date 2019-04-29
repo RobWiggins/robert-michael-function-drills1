65e18bca0e65c609c8eb597ac5d24d8cf0858be2
m-cal <iengineersoftware@gmail.com>
2019-04-29 15:58:29 -0400

    finished step 6

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
+function getYearOfBirth(age) {
+    return 2019 - age;
+}
+
 function createGreeting(name, age) {
-  let yearOfBirth = 2019 - age;
-  return `Hi, my name is ${name} and my age is ${age} and I was born in ${yearOfBirth}.`;
+  return `Hi, my name is ${name} and my age is ${age} and I was born in ${getYearOfBirth(age)}.`;
 }
 
 const aSentence = createGreeting('Mike', 23);
 console.log(aSentence);
-
 
