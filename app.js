8eb9c8899e5b25b5db97f865f65d93292ad1d887
Robert Wiggins <wigginsro11@gmail.com>
2019-04-29 15:38:06 -0400

    new function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict';
 function createGreeting() {
-  console.log('Hi, my name is Rob and I\'m 26');
+  return 'Hi, my name is Rob and I\'m 26';
 }
 
-createGreeting();
+const aSentence = createGreeting();
+console.log(aSentence);
